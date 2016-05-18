b3fd815b03a33965ac650fef9706bace10231e89
ryanisaacg <ryanisaacg@gmail.com>
2016-05-18 09:55:08 -0400

    Created empty libGDX project

---
+++ desktop/src/com/fwumdesoft/project8/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.fwumdesoft.project8.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.fwumdesoft.project8.Project8;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Project8(), config);
+	}
+}
