a92a16960f25fbc91af3e5139120ecb613a46aca
EpicPieGuy <ryanisaacg@gmail.com>
2016-05-22 17:18:53 -0400

    Created an Inventory class; closed #10

---
+++ core/src/com/fwumdesoft/project8/Inventory.java
@@ -0,0 +1,14 @@
+package com.fwumdesoft.project8;
+/**
+ * A class to represent the player's picked up items
+ */
+public class Inventory {
+	/**
+	 * The number of resistors the player currently has
+	 */
+	public int resistors;
+	/**
+	 * The number of computer chips the player currently has
+	 */
+	public int computerChips;
+}
