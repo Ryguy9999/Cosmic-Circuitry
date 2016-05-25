3dc720fce94ef780991b29d0e0ee4584fb8d5e8d
EpicPieGuy <ryanisaacg@gmail.com>
2016-05-25 16:26:59 -0400

    Added a boolean to tell if a component is a normal resistor or is a lamp

--- core/src/com/fwumdesoft/project8/CircuitComponent.java
+++ core/src/com/fwumdesoft/project8/CircuitComponent.java
@@ -2,12 +2,14 @@ package com.fwumdesoft.project8;
 
 public class CircuitComponent{
 	public enum Type {WIRE, BATTERY, RESISTOR };
+	public boolean isLamp;
 	public boolean isChangeable;
 	Type type;
 	public double voltageDif, current, resistance;
 	
 	public CircuitComponent(){
 		isChangeable = false;
+		isLamp = false;
 		voltageDif = current = resistance = 0;
 	}
 }
