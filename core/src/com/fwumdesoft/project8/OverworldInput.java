43e21313708a722c4b2d8b9097dc7c9c36510478
EpicPieGuy <ryanisaacg@gmail.com>
2016-05-27 09:40:06 -0400

    Added a button input to interact with item in the overworld; close #12

--- core/src/com/fwumdesoft/project8/OverworldInput.java
+++ core/src/com/fwumdesoft/project8/OverworldInput.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.InputAdapter;
 
 public class OverworldInput extends InputAdapter {
 	private Overworld world;
+	private int faceX, faceY;
 	
 	public OverworldInput(Overworld world) {
 		this.world = world;
@@ -13,20 +14,29 @@ public class OverworldInput extends InputAdapter {
 	public boolean keyDown (int keycode) {
 		switch(keycode) {
 		case Keys.A:
-			world.movePlayer(-1, 0);
+			move(-1, 0);
 			break;
 		case Keys.D:
-			world.movePlayer(1, 0);
+			move(1, 0);
 			break;
 		case Keys.W:
-			world.movePlayer(0, 1);
+			move(0, 1);
 			break;
 		case Keys.S:
-			world.movePlayer(0, -1);
+			move(0, -1);
+			break;
+		case Keys.SPACE:
+			world.interactWith(faceX, faceY);
 			break;
 		default:
 			return false;
 		}
 		return true;
+	}
+	
+	private void move(int x, int y) {
+		world.movePlayer(x, y);
+		faceX = x;
+		faceY = y;
 	}
 }
