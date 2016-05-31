88dd0d83deec492dbfec52c5cc44dee54d261274
Ryguy9999 <ryancirincione@aol.com>
2016-05-31 12:09:35 -0400

    Added Circuit

---
+++ core/src/com/fwumdesoft/project8/Circuit.java
@@ -0,0 +1,24 @@
+package com.fwumdesoft.project8;
+
+public class Circuit
+{
+	public CircuitComponent[][] grid;
+	public int goalLamps;
+	
+	public Circuit(CircuitComponent[][] grid, int goalLamps)
+	{
+		this.grid = grid;
+		this.goalLamps = goalLamps;
+	}
+	
+	public boolean isSolved()
+	{
+		int count = 0;
+		for(CircuitComponent[] row : grid)
+			for(CircuitComponent comp : row)
+				if(comp.isActive)
+					count++;
+		
+		return count == goalLamps;
+	}
+}
