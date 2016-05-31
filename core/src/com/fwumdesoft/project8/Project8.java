60c75559530c2cd73d40ecbee61c626f53acaf69
Alex Berman <inser7name@gmail.com>
2016-05-31 12:47:58 -0400

    closed #24 but in a stupid way, we also lost Scotty in the process

--- core/src/com/fwumdesoft/project8/Project8.java
+++ core/src/com/fwumdesoft/project8/Project8.java
@@ -54,7 +54,7 @@ public class Project8 extends ApplicationAdapter {
 		assets.finishLoading();
 		manualCleanup.add(assets);
 		
-		world = new Overworld(200, assets.getAll(CircuitComponent[][].class, new Array<>()));
+		world = new Overworld(500, assets.getAll(CircuitComponent[][].class, new Array<>()));
 		rend = new Renderer(batch, new BitmapFont(), assets, 32, 64, 640, 480);
 
 		inventory = new Inventory();
