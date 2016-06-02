2b4bf19a7798e30273fa0f7a91030c1e6d9b78e3
EpicPieGuy <ryanisaacg@gmail.com>
2016-06-02 15:46:28 -0400

    Rolled back unnecessary UUID change in Circuit

--- core/src/com/fwumdesoft/project8/Circuit.java
+++ core/src/com/fwumdesoft/project8/Circuit.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class Circuit implements Serializable
 {
-	private static final long serialVersionUID = 2L;
+	private static final long serialVersionUID = 1L;
 	public CircuitComponent[][] grid;
 	public String name;
 	public int goalLamps;
