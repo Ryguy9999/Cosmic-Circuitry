6b3a12cab18d232cebb317c6c4f6fa4bcd55c28f
EpicPieGuy <ryanisaacg@gmail.com>
2016-06-02 15:31:18 -0400

    Made Circuit serializable

--- core/src/com/fwumdesoft/project8/Circuit.java
+++ core/src/com/fwumdesoft/project8/Circuit.java
@@ -1,7 +1,10 @@
 package com.fwumdesoft.project8;
 
-public class Circuit
+import java.io.Serializable;
+
+public class Circuit implements Serializable
 {
+	private static final long serialVersionUID = 1L;
 	public CircuitComponent[][] grid;
 	public int goalLamps;
 
