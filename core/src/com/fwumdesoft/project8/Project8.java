26106b595a29bf97788ff43b346d42a692fd0b9e
Ryan Goldstein <ryanisaacg@gmail.com>
2016-06-03 21:44:42 -0400

    Added a smoke particle type for #67

--- core/src/com/fwumdesoft/project8/Project8.java
+++ core/src/com/fwumdesoft/project8/Project8.java
@@ -53,8 +53,21 @@ public class Project8 extends ApplicationAdapter
 			p.position.setVertices(new float[]{0,0, 4,0, 0,4, 4,4});
 			p.position.setScale(1, 1);
 			p.position.setOrigin(2, 2);
-			p.lifetime = 20;
+			p.lifetime = 10 + (int)(Math.random() * 15);
 			p.texture = new TextureRegion(assets.get("electricity_particle.png", Texture.class));
+			p.velocity.set(((float)Math.random() - 0.5f) * 5, ((float)Math.random() - 0.5f) * 5);
+			p.rotationalVelocity = 30;
+			p.scaleVelocity = 0;
+		});
+		ParticleSystem.addParticleType("smoke", p -> {
+			p.position.setVertices(new float[]{0,0, 16,0, 0,16, 16,16});
+			p.position.setScale(1, 1);
+			p.position.setOrigin(8, 8);
+			p.rotationalVelocity = (int)(Math.random() * 20);
+			p.lifetime = (int)(Math.random() * 25);
+			p.deltaOpacity = -0.05f;
+			p.scaleVelocity = (float)Math.random() / 2;
+			p.texture = new TextureRegion(assets.get("smoke_puff.png", Texture.class));
 			p.velocity.set(((float)Math.random() - 0.5f) * 5, ((float)Math.random() - 0.5f) * 5);
 			p.rotationalVelocity = 30;
 			p.scaleVelocity = 0;
