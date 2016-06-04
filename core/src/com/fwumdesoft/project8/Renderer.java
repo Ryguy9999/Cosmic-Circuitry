835744c9ec9000efe12bb08910dc063d92398646
EpicPieGuy <ryanisaacg@gmail.com>
2016-06-04 09:04:37 -0400

    Greatly reduced particle frequency for broken circuits; closed #73

--- core/src/com/fwumdesoft/project8/Renderer.java
+++ core/src/com/fwumdesoft/project8/Renderer.java
@@ -275,7 +275,7 @@ public class Renderer
 					batch.draw(region, drawX, drawY);
 				} else
 				{
-					if(comp.isChangeable && comp.type == null && Math.random() < 0.05)
+					if(comp.isChangeable && comp.type == null && Math.random() < 0.005)
 						ParticleSystem.burst("electricity", drawX + componentSize / 2, drawY + componentSize / 2, 5);
 					int rotation = (top && bottom) ? 90 : 0;
 					Texture tex;
