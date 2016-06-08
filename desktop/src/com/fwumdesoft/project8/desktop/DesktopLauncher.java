a4a1d462f334acd6c5b7dca76a52f1f0b3e63c31
EpicPieGuy <ryanisaacg@gmail.com>
2016-06-07 20:05:45 -0400

    Added windowing icons to the game

--- desktop/src/com/fwumdesoft/project8/desktop/DesktopLauncher.java
+++ desktop/src/com/fwumdesoft/project8/desktop/DesktopLauncher.java
@@ -1,12 +1,18 @@
 package com.fwumdesoft.project8.desktop;
 
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.fwumdesoft.project8.App;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
+public class DesktopLauncher 
+{
+	public static void main(String[] arg) 
+	{
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.addIcon("icon-64.png", FileType.Internal);
+		config.addIcon("icon-32.png", FileType.Internal);
+		config.addIcon("icon-16.png", FileType.Internal);
 		new LwjglApplication(new App(), config);
 	}
 }
